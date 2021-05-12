8b13cf9466cacc28507e5ee1fcbc5a1a5fa1698f
Miikka <miikkak@tuta.io>
2021-05-11 19:03:07 -0600

    finished

--- src/components/Buttons/UISocialMediaButton.js
+++ src/components/Buttons/UISocialMediaButton.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import  useStyles  from '../../styles/styles';
-import { Button } from '@material-ui/core';
-import  SvgGoogle  from '../../styles/icons/SvgGoogle.js';
-import  SvgFacebook  from '../../styles/icons/SvgFacebook.js';
+import React from "react";
+import useStyles from "../../styles/styles";
+import { Button } from "@material-ui/core";
+import SvgGoogle from "../../styles/icons/SvgGoogle.js";
+import SvgFacebook from "../../styles/icons/SvgFacebook.js";
 
 const UISocialMediaButton = ({ vendor }) => {
-    console.log(vendor)
-    const classes = useStyles();
-    return (
-        <>
-            { vendor === "facebook" ? <Button startIcon={<SvgFacebook />} className={classes.socialMediaButton}>FACEBOOK</Button> :  <Button startIcon={<SvgGoogle />} className={classes.socialMediaButton}>GOOGLE</Button>}
-        </>
-    )
-}
+  const classes = useStyles();
+  return (
+    <>
+      {vendor === "facebook" ? (
+        <Button
+          startIcon={<SvgFacebook />}
+          className={classes.socialMediaButton}
+        >
+          FACEBOOK
+        </Button>
+      ) : (
+        <Button startIcon={<SvgGoogle />} className={classes.socialMediaButton}>
+          GOOGLE
+        </Button>
+      )}
+    </>
+  );
+};
 
 export default UISocialMediaButton;
+
